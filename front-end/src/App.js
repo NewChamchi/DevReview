a5c7fecfef406606f01c0d96def907ac9e8033c4
NewChamchi <51375290+NewChamchi@users.noreply.github.com>
2023-04-02 13:19:54 +0900

    테스트 코드 파일 등록

---
+++ front-end/src/App.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(
+                    "http://localhost:8080/api/test"
+                );
+                setData(response.data);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            <h1>React & Spring Boot Test</h1>
+            {data ? (
+                <p>Data from backend: {data.message}</p>
+            ) : (
+                <p>Loading...</p>
+            )}
+        </div>
+    );
+}
+
+export default App;
