d13fc48de655a7544cde46b0202b15dc57ba4ff1
NewChamchi <51375290+NewChamchi@users.noreply.github.com>
2023-04-02 13:21:38 +0900

    api 이름 변경

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,7 +8,7 @@ function App() {
         const fetchData = async () => {
             try {
                 const response = await axios.get(
-                    "http://localhost:8080/api/test"
+                    "http://localhost:8080/api/add"
                 );
                 setData(response.data);
             } catch (error) {
