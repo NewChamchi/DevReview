db4ac8346da8c2faf186a9f36a9cd371d73e0f8f
Jae Hun Heo <tofhdnsckacl123@daum.net>
2023-05-16 20:01:15 +0900

    docker 및 github action(주석처리함) 파일 추가

---
+++ DataBase/init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    email VARCHAR(50)
+);
+
+INSERT INTO users (username, password, email)
+VALUES ('testuser', 'testpass', 'testuser@example.com');
